fb0b6488b9eb4b7d2b74bf3afa38e8acd43d4d12
zhangbicheng <zhangbicheng@qutoutiao.net>
2020-09-27 01:29:08 +0800

    remove notes

--- drivers/kafka.go
+++ drivers/kafka.go
@@ -35,27 +35,4 @@ func NewConsumer() sarama.Consumer {
 	}
 
 	return consumer
-	// 	fmt.Println("connnect success...")
-	// 	defer consumer.Close()
-	// 	partitions, err := consumer.Partitions("revolution")
-	// 	if err != nil {
-	// 		fmt.Println("geet partitions failed, err:", err)
-	// 		return nil
-	// 	}
-
-	// 	for _, p := range partitions {
-	// 		partitionConsumer, err := consumer.ConsumePartition("revolution", p, sarama.OffsetOldest)
-	// 		if err != nil {
-	// 			fmt.Println("partitionConsumer err:", err)
-	// 			continue
-	// 		}
-	// 		wg.Add(1)
-	// 		go func() {
-	// 			for m := range partitionConsumer.Messages() {
-	// 				fmt.Printf("key: %s, text: %s, offset: %d\n", string(m.Key), string(m.Value), m.Offset)
-	// 			}
-	// 			wg.Done()
-	// 		}()
-	// 	}
-	// 	wg.Wait()
 }
